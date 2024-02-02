dab89fc3e407eacc69f4048a9bd578f7dc66a39d
talismanchik <talismanchik.by@gmail.com>
2024-02-02 20:44:50 +0300

    create: Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button>Button without link</Button>
+      <Button as={'a'} href={''}>
+        Button with link
+      </Button>
+    </div>
+  )
 }
---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import clsx from 'clsx'
+
+import s from './button.module.scss'
+
+type Props<T extends ElementType = 'button'> = {
+  as?: T
+  className?: string
+  fullWidth?: boolean
+  variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(props: Props<T>) => {
+  const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+
+  const classNames = {
+    component: clsx(s.button, s[variant], fullWidth && s.fullWidth, className),
+  }
+
+  return <Component className={classNames.component} {...rest} />
+}
