d06c7a6447b6558a876904f7e63e2f2b08c14283
talismanchik <talismanchik.by@gmail.com>
2024-02-02 14:45:53 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
+
 import { App } from './App'
 
 createRoot(document.getElementById('root')!).render(
