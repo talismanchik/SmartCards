9228f91f3ae55fe61b42fd9c7fa8ee85feefed6c
NatKhomich <yalapulya94@mail.ru>
2024-02-06 11:23:24 +0600

    #13 init: select component

---
+++ src/components/ui/select/select.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Select } from './'
+
+const meta = {
+  component: Select,
+  tags: ['autodocs'],
+  title: 'Components/Select',
+} satisfies Meta<typeof Select>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
