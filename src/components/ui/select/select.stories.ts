e0ecde03d910c97ac54159723c175952b275ae28
NatKhomich <yalapulya94@mail.ru>
2024-02-07 14:57:29 +0600

    #13  feat: select component functional

--- src/components/ui/select/select.stories.ts
+++ src/components/ui/select/select.stories.ts
@@ -1,16 +1,35 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Select } from './'
+import { SelectDemo } from '@/components/ui/select/select'
 
 const meta = {
-  component: Select,
+  component: SelectDemo,
   tags: ['autodocs'],
-  title: 'Components/Select',
-} satisfies Meta<typeof Select>
+  title: 'Components/SelectDemo',
+} satisfies Meta<typeof SelectDemo>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    options: [
+      { title: 'value-1', value: '1' },
+      { title: 'value-2', value: '2' },
+      { title: 'value-3', value: '3' },
+    ],
+    placeholder: 'Select-box',
+  },
+}
+
+export const DefaultWithLabel: Story = {
+  args: {
+    label: 'Select-box',
+    options: [
+      { title: 'value-1', value: '1' },
+      { title: 'value-2', value: '2' },
+      { title: 'value-3', value: '3' },
+    ],
+    placeholder: 'D',
+  },
 }
--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,3 +1,77 @@
-export const Select = () => {
-  return <div>select</div>
+import { ComponentPropsWithoutRef, ElementRef, FC, forwardRef } from 'react'
+
+import { ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons'
+import * as Select from '@radix-ui/react-select'
+
+import s from './select.module.scss'
+
+type OptionsProps = {
+  title: string
+  value: string
 }
+
+type SelectProps = {
+  className?: string
+  label?: string
+  options?: OptionsProps[]
+  placeholder?: string
+} & ComponentPropsWithoutRef<typeof Select.Root>
+
+export const SelectDemo: FC<SelectProps> = (props: SelectProps) => {
+  const { className, disabled, label, onValueChange, options, placeholder, value, ...restProps } =
+    props
+
+  return (
+    <Select.Root
+      disabled={disabled}
+      onValueChange={onValueChange}
+      required={restProps.required}
+      value={value}
+    >
+      {label && <label> {label} </label>}
+      <Select.Trigger aria-label={'select'} className={s.trigger}>
+        <Select.Value placeholder={'Select-box'} />
+        <Select.Icon className={'SelectIcon'}>
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content className={s.content} position={'popper'}>
+          <Select.ScrollUpButton className={'SelectScrollButton'}>
+            <ChevronUpIcon />
+          </Select.ScrollUpButton>
+          <Select.Viewport className={'SelectViewport'}>
+            <Select.Group>
+              {options?.map(option => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.title}
+                </SelectItem>
+              ))}
+            </Select.Group>
+            <Select.Separator className={'SelectSeparator'} />
+          </Select.Viewport>
+          <Select.ScrollDownButton className={'SelectScrollButton'}>
+            <ChevronDownIcon />
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  )
+}
+
+type SelectItemProps = {
+  className?: string
+  variant?: string
+} & ComponentPropsWithoutRef<typeof Select.Item>
+
+export const SelectItem = forwardRef<ElementRef<typeof Select.Item>, SelectItemProps>(
+  ({ children, className, variant = 'default', ...restProps }, ref): JSX.Element => {
+    return (
+      <Select.Item ref={ref} {...restProps}>
+        <Select.ItemText>
+          <p>{children}</p>
+        </Select.ItemText>
+      </Select.Item>
+    )
+  }
+)
