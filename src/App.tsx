4d78293fd1043727d085e5f7ea2940a7bbac9ca6
talismanchik <talismanchik.by@gmail.com>
2024-02-07 11:25:04 +0300

    feat: #15-assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,3 @@
-import { Icon } from '@/components/ui/icon/Icon'
-
 export function App() {
-  return (
-    <div style={{ alignItems: 'center', display: 'flex', gap: '5px', justifyContent: 'center' }}>
-      <Icon iconId={'log_out'} />
-      <Icon iconId={'home'} />
-      <Icon iconId={'home_outline'} />
-      <Icon iconId={'search'} />
-      <Icon iconId={'eye'} />
-      <Icon iconId={'eye_outline'} />
-      <Icon iconId={'eye_off'} />
-      <Icon iconId={'eye_off_outline'} />
-      <Icon iconId={'checkbox_true'} />
-      <Icon iconId={'checkbox_false'} />
-    </div>
-  )
+  return <div>Hello!</div>
 }
