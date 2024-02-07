bc7d2875ab57ef2635cfe12d719c5c308c95ca23
Nadufa <nadufa18404@gmail.com>
2024-02-07 15:13:58 +0300

    fix: #8-slider change some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Icon } from '@/components/ui/icon/Icon'
+
 export function App() {
-  return <div>Hello!</div>
+  return (
+    <div>
+      <Icon height={'24'} iconId={'eye'} width={'24'} />
+      <Icon height={'24'} iconId={'eye_off'} width={'24'} />
+      <Icon height={'24'} iconId={'eye_outline'} width={'24'} />
+      <Icon height={'24'} iconId={'eye_off_outline'} width={'24'} />
+    </div>
+  )
 }
