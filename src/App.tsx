c6a8967e151fa521b1948362634f0d201f7254ee
talismanchik <talismanchik.by@gmail.com>
2024-02-08 10:02:53 +0300

    merge: slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import { Icon } from '@/components/ui/icon/Icon'
-
 export function App() {
-  return (
-    <div>
-      <Icon height={'24'} iconId={'eye'} width={'24'} />
-      <Icon height={'24'} iconId={'eye_off'} width={'24'} />
-      <Icon height={'24'} iconId={'eye_outline'} width={'24'} />
-      <Icon height={'24'} iconId={'eye_off_outline'} width={'24'} />
-    </div>
-  )
+  return <div>hello!</div>
 }
