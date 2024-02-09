1c9b5692b4db452ac10f6d0fda5bd5420e4b36e6
Nadufa <nadufa18404@gmail.com>
2024-02-09 17:37:43 +0300

    refactor: #11-radioGroup change structure, styles, storybook

--- src/components/ui/radioGroup/RadioGroup.stories.tsx
+++ src/components/ui/radioGroup/RadioGroup.stories.tsx
@@ -1,22 +1,46 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 
-import { RadioGroupComponent } from '@/components/ui/radioGroup/RadioGroupComponent'
+import { useState } from 'react'
+
+import { InitialStateType, RadioGroup } from '@/components/ui/radioGroup/RadioGroup'
 
 const meta = {
   argTypes: {
     disabled: {
       defaultValue: false,
+      options: [true, false],
       type: 'boolean',
     },
   },
-  component: RadioGroupComponent,
+  component: RadioGroup,
   tags: ['autodocs'],
   title: 'Components/RadioGroup',
-} satisfies Meta<typeof RadioGroupComponent>
+} satisfies Meta<typeof RadioGroup>
 
 export default meta
-type Story = StoryObj<typeof meta>
+const initialState: InitialStateType = [
+  { id: '1', value: 'Did not know' },
+  { id: '2', value: 'Forgot' },
+  { id: '3', value: 'A lot of thought' },
+  { id: '4', value: 'Confused' },
+  { id: '5', value: 'Knew the answer' },
+]
 
-export const Base: Story = {
-  args: {},
+export const Base = {
+  render() {
+    const [value, setValue] = useState('Did not know')
+
+    return (
+      <RadioGroup disabled={false} onValueChange={setValue} value={value} values={initialState} />
+    )
+  },
+}
+
+export const Disabled = {
+  args: {
+    disabled: true,
+    onValueChange: () => {},
+    value: 'Did not know',
+    values: initialState,
+  },
 }
