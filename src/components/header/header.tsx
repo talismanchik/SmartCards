6410990b035b7c425ee78c237a8b68e3c966e788
talismanchik <talismanchik.by@gmail.com>
2024-02-09 18:43:24 +0300

    refactor: Header without DropDownMenu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,9 @@
 import { animateScroll as scroll } from 'react-scroll'
 
+import { AvatarDefault } from '@/assets/avatar/avatarDefoult'
 import { Logo } from '@/assets/logo/logo'
 import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
 import clsx from 'clsx'
 
 import s from './header.module.scss'
@@ -14,19 +16,33 @@ export const Header = ({ isAuth }: Props) => {
   const classNames = {
     container: clsx(s.container),
     header: clsx(s.header),
+    logo: clsx(s.logo),
+    userAvatar: clsx(s.userAvatar),
+    userInfo: clsx(s.userInfo),
+    userName: clsx(s.userName),
   }
 
   return (
     <header className={classNames.header}>
       <div className={classNames.container}>
         <a
+          className={classNames.logo}
           onClick={() => {
             scroll.scrollToTop()
           }}
         >
           <Logo />
         </a>
-        {isAuth ? <div>auth</div> : <Button variant={'secondary'}>Sign in</Button>}
+        {isAuth ? (
+          <div className={classNames.userInfo}>
+            <Typography className={classNames.userName} variant={'subtitle1'}>
+              User Name
+            </Typography>
+            <AvatarDefault className={classNames.userAvatar} />
+          </div>
+        ) : (
+          <Button variant={'secondary'}>Sign in</Button>
+        )}
       </div>
     </header>
   )
