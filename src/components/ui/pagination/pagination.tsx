c72fdb0f52aa2a7c9a65eafa7d291839f741034e
NatKhomich <yalapulya94@mail.ru>
2024-02-12 15:26:44 +0600

    #9 feat: styles pagination, styles select

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -3,6 +3,7 @@ import { usePagination } from '@/components/ui/pagination/usePagination'
 import { Select } from '@/components/ui/select'
 import { Typography } from '@/components/ui/typography'
 import { SelectProps } from '@radix-ui/react-select'
+import clsx from 'clsx'
 
 import s from './pagination.module.scss'
 
@@ -22,12 +23,25 @@ export const Pagination = ({
   totalCount,
   ...restProps
 }: PaginationProps) => {
+  const classNames = {
+    item(currentItem: number) {
+      return clsx(s.button, currentItem === currentPage && s.activeItem)
+    },
+  }
   const totalPageCount = Math.ceil(totalCount / pageSize)
   const paginationItems = usePagination({
     currentPage,
     siblingCount,
     totalPageCount,
   })
+
+  const onPageSizeChange = (newPageSize: number) => {
+    Math.ceil(totalCount / newPageSize)
+    pageSize !== newPageSize && onPageChange(1)
+    pageSize !== newPageSize &&
+      restProps.onValueChange &&
+      restProps.onValueChange(newPageSize.toString())
+  }
 
   const onPrevPage = () => {
     if (currentPage !== 1) {
@@ -41,6 +55,12 @@ export const Pagination = ({
     }
   }
 
+  const optionsSelect = [
+    { title: '100', value: '100' },
+    { title: '50', value: '50' },
+    { title: '20', value: '20' },
+  ]
+
   return (
     <div className={s.root}>
       <div className={s.paginationContainer}>
@@ -52,14 +72,20 @@ export const Pagination = ({
           {paginationItems?.map((num, index) => {
             if (num === '...') {
               return (
-                <Typography as={'span'} key={index} tabIndex={-1} variant={'body2'}>
+                <Typography
+                  as={'span'}
+                  className={s.dots}
+                  key={index}
+                  tabIndex={-1}
+                  variant={'body2'}
+                >
                   {num}
                 </Typography>
               )
             } else {
               return (
                 <button
-                  className={s.button}
+                  className={classNames.item(+num)}
                   key={index}
                   onClick={() => onPageChange(+num)}
                   tabIndex={0}
@@ -82,7 +108,13 @@ export const Pagination = ({
         <Typography as={'span'} variant={'body2'}>
           Показать
         </Typography>
-        <Select className={s.select} placeholder={'100'} {...restProps} />
+        <Select
+          className={s.select}
+          placeholder={'100'}
+          {...restProps}
+          onValueChange={newValue => onPageSizeChange(+newValue)}
+          options={optionsSelect}
+        />
         <Typography as={'span'} variant={'body2'}>
           на странице
         </Typography>
