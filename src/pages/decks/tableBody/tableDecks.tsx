701ce579f98db254afb44fc38a7182d808d5a6cf
NatKhomich <yalapulya94@mail.ru>
2024-02-26 10:45:32 +0600

    #25 feat: add cards router

--- src/pages/decks/tableBody/tableDecks.tsx
+++ src/pages/decks/tableBody/tableDecks.tsx
@@ -1,4 +1,4 @@
-import defaultImage from '@/assets/photo-camera.png'
+// import defaultImage from '@/assets/photo-camera.png'
 import { Icon } from '@/components/ui/icon/Icon'
 import { TableDataCell, TableRow } from '@/components/ui/table/tableConstructor'
 import { Typography } from '@/components/ui/typography'
@@ -30,7 +30,7 @@ export const TableDecks = ({ decks }: props) => {
           <TableRow key={deck.id}>
             <TableDataCell>
               <span className={styles.nameWrapper}>
-                <img alt={'deck photo'} className={s.tableImage} src={defaultImage} />
+                {/*<img alt={'deck photo'} className={s.tableImage} src={defaultImage} />*/}
                 <Typography variant={'body2'}>{deck.name}</Typography>
               </span>
             </TableDataCell>
