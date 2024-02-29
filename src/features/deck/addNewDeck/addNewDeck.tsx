9f89e17781626718b44f425651bea05f72816680
NatKhomich <yalapulya94@mail.ru>
2024-02-29 18:48:55 +0600

    #27 feat: logic add File

--- src/features/deck/addNewDeck/addNewDeck.tsx
+++ src/features/deck/addNewDeck/addNewDeck.tsx
@@ -1,4 +1,5 @@
-import { useForm, useFormState } from 'react-hook-form'
+import { ChangeEvent, useRef, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import { ControlledCheckbox } from '@/components/controlled/controlledCheckbox'
 import { ControlledInput } from '@/components/controlled/controlledInput'
@@ -18,33 +19,72 @@ type AddNewDeckProps = {
 export type AddNewDeckFormValues = z.infer<typeof addNewDeckSchema>
 
 const addNewDeckSchema = z.object({
-  isPrivate: z.boolean().optional(),
-  nameDeck: z.string(),
+  isPrivate: z.boolean(),
+  name: z.string(),
 })
 
 export const AddNewDeck = ({ isOpen, onOpenChange }: AddNewDeckProps) => {
-  const { control, handleSubmit } = useForm<AddNewDeckFormValues>({
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<AddNewDeckFormValues>({
+    defaultValues: {
+      isPrivate: false,
+      name: '',
+    },
     resolver: zodResolver(addNewDeckSchema),
   })
-
-  const { errors } = useFormState({ control })
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const [cover, setCover] = useState<File | null | string>(null)
 
   console.log(errors)
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0]
+
+    setCover(selectedFile || null)
+    console.log(selectedFile)
+  }
+
+  const imageUrl = cover ? URL.createObjectURL(cover as File) : undefined
   const onSubmit = (data: AddNewDeckFormValues) => {
-    console.log(data)
+    const formData = new FormData()
+
+    formData.append('name', data.name)
+    formData.append('isPrivate', data.isPrivate.toString())
+    cover && formData.append('cover', cover || '')
+
+    formData.forEach((value, key) => {
+      console.log(key, value)
+    })
+  }
+
+  const openFileInput = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click()
+    }
   }
 
   return (
     <Modal className={s.wrapper} onOpenChange={onOpenChange} open={isOpen} title={'Add New Deck'}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <ControlledInput
-          className={s.input}
-          control={control}
-          label={'Name Deck'}
-          name={'nameDeck'}
-        />
-        <Button className={s.button} fullWidth type={'button'} variant={'secondary'}>
+        <ControlledInput className={s.input} control={control} label={'Name Deck'} name={'name'} />
+        {cover && (
+          <img
+            alt={'cover'}
+            src={imageUrl}
+            style={{ height: '200px', objectFit: 'cover', width: '100%' }}
+          />
+        )}
+        <Button
+          className={s.button}
+          fullWidth
+          onClick={openFileInput}
+          type={'button'}
+          variant={'secondary'}
+        >
+          <input onChange={handleFileChange} ref={fileInputRef} type={'file'} />
           <Icon iconId={'image_outline'} />
           <Typography variant={'subtitle2'}>Upload Image</Typography>
         </Button>
