0a6ac5b497890839beabb0d2d40eaa6af617f5e9
NatKhomich <yalapulya94@mail.ru>
2024-03-18 16:21:12 +0300

    #29 init: create imageContainer

---
+++ src/components/ui/imageContainer/imageContainer.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, useRef } from 'react'
+
+import { Button } from '@/components/ui/button'
+import { Icon } from '@/components/ui/icon/Icon'
+import { InputFile } from '@/components/ui/inputFile'
+import { Typography } from '@/components/ui/typography'
+
+import s from './imageContainer.module.scss'
+
+type Props = {
+  handleSaveFile: (file: File | undefined) => void
+  imageUrl: File | null | string | undefined
+}
+export const ImageContainer = ({ handleSaveFile, imageUrl }: Props) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+
+  const openFileInput = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click()
+    }
+  }
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    handleSaveFile(e.target.files?.[0])
+  }
+
+  return (
+    <div>
+      {imageUrl && <img alt={'cover'} className={s.coverImage} src={imageUrl as string} />}
+      <Button
+        className={s.button}
+        fullWidth
+        onClick={openFileInput}
+        type={'button'}
+        variant={'secondary'}
+      >
+        <InputFile handleFileChange={handleFileChange} ref={fileInputRef} />
+        <Icon iconId={'image_outline'} />
+        <Typography variant={'subtitle2'}>Upload Image</Typography>
+      </Button>
+    </div>
+  )
+}
--- src/features/deck/deckForm/deckForm.tsx
+++ src/features/deck/deckForm/deckForm.tsx
@@ -1,13 +1,11 @@
-import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import { ControlledCheckbox } from '@/components/controlled/controlledCheckbox'
 import { ControlledInput } from '@/components/controlled/controlledInput'
 import { Button } from '@/components/ui/button'
-import { Icon } from '@/components/ui/icon/Icon'
-import { InputFile } from '@/components/ui/inputFile'
+import { ImageContainer } from '@/components/ui/imageContainer'
 import { Modal } from '@/components/ui/modal/Modal'
-import { Typography } from '@/components/ui/typography'
 import { EditValues } from '@/features/deck/updateDeck'
 import { CreateDeckArgs } from '@/services/decks/decks.types'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -49,19 +47,10 @@ export const DeckForm = ({
     resetField('name', { defaultValue: editValues?.name })
   }, [editValues, resetField])
 
-  const fileInputRef = useRef<HTMLInputElement | null>(null)
   const [cover, setCover] = useState<File | null | string>(null)
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0]
-
-    setCover(selectedFile || null)
-  }
-
-  const openFileInput = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click()
-    }
+  const handleSaveFile = (file: File | undefined) => {
+    setCover(file || null)
   }
 
   const imageUrl = cover ? URL.createObjectURL(cover as File) : editValues?.cover
@@ -94,18 +83,7 @@ export const DeckForm = ({
     <Modal className={s.wrapper} onOpenChange={onClosedModal} open={isOpen} title={title}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <ControlledInput className={s.input} control={control} label={'Name Deck'} name={'name'} />
-        {imageUrl && <img alt={'cover'} className={s.coverImage} src={imageUrl as string} />}
-        <Button
-          className={s.button}
-          fullWidth
-          onClick={openFileInput}
-          type={'button'}
-          variant={'secondary'}
-        >
-          <InputFile handleFileChange={handleFileChange} ref={fileInputRef} />
-          <Icon iconId={'image_outline'} />
-          <Typography variant={'subtitle2'}>Upload Image</Typography>
-        </Button>
+        <ImageContainer handleSaveFile={handleSaveFile} imageUrl={imageUrl} />
         <ControlledCheckbox control={control} label={'Private deck'} name={'isPrivate'} />
         <div className={s.buttonWrapper}>
           <Button onClick={() => onClosedModal(false)} type={'button'} variant={'secondary'}>
