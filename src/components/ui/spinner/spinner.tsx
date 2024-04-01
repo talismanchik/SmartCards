437ae27976f083f1a48d5ef8b79be91cb637c122
talismanchik <talismanchik.by@gmail.com>
2024-04-01 21:37:51 +0300

    correct profile

--- src/components/ui/spinner/spinner.tsx
+++ src/components/ui/spinner/spinner.tsx
@@ -2,7 +2,7 @@ import s from './spinner.module.scss'
 export const Spinner = () => {
   return (
     <div className={s.wrapper}>
-      <span className={s.loader}> </span>
+      <span className={s.loader}></span>
     </div>
   )
 }
